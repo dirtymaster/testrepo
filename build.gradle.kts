bd9edcd76eb2e1f5ea24aa6132120e14ee6a8c0c
Rafael <kunakov.rafael@gmail.com>
2025-11-08 18:39:07 +0300

    new commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+    // подключи зависимость для RandomStringUtils
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
